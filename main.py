10024c110d5eb8d56138d8320e2510d23da316b7
Ummer Memon <umemon0001@gmail.com>
2025-08-09 16:25:50 +0530

    template setup done

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
 @app.get("/")
-def home():
-    return {'message': "Hello World!"}
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
