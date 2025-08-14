6049da7a32786955eeebac651f8ee61555a4bd19
Ummer Memon <umemon0001@gmail.com>
2025-08-14 15:30:07 +0530

    get tasks api done

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
+from pymongo import MongoClient
+from pydantic import BaseModel
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
+client = MongoClient('mongodb://localhost:27017/')
+task_db = client['task_db']
+task_collection = task_db['tasks']
+
+class Task(BaseModel):
+    title: str
+    description: str
+    completed: bool = False
+
+def serialize_task(task):
+    return {
+        "id": str(task["_id"]),
+        "title": task["title"]
+    }
+
 
 @app.get("/")
 def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return {'hello':'world!'}
+
+@app.get("/tasks/")
+def get_tasks():
+    tasks = list(task_collection.find())
+    for task in tasks:
+        task["_id"] = str(task["_id"])
+    return tasks
+
+@app.post("tasks/add")
+def add_task(task: Task):
+    result = task_collection.insert_one(task.dict())
+    return {'message': 'task added'}
