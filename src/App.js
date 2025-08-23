34aae58e724ff2caf43536146aca556571abde49
Ummer Memon <umemon0001@gmail.com>
2025-08-23 22:22:02 +0530

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { PencilSimple, Power } from "phosphor-react";
+import { useState } from "react";
 
 function App() {
+  const [showCard, setShowCard] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="bg-blue-500 flex justify-between items-center p-3 shadow-xl">
+        <div className="logo"><h2 className="text-white text-2xl font-semibold">TaskList</h2></div>
+        <div className="profile cursor-pointer">
+          <img src="/assets/alex.jpg" className="rounded-full hover:border" onClick={() => setShowCard((prev) => !prev) } alt='image' width={50} />
+          {showCard && (
+
+            <div className="absolute right-3 mt-1 border card bg-white p-2 rounded-xl">
+          <ul>
+            <li className="hover:bg-gray-100 hover:rounded-xl p-2 transition flex space-x-2 items-center"><PencilSimple size={20} weight="light" /><a href="#">Edit Profile</a></li>
+            <li className="hover:bg-gray-100 hover:rounded-xl p-2 transition flex space-x-2 items-center"><Power size={20} weight="light" /><a href="#">Signout</a></li>
+          </ul>
+          </div>
+          )}
+        </div>
+      </nav>
+    </>
   );
 }
 
